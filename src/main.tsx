99b5bd5c34affcf638f46562d4948c7401267883
Alexs779 <torentino2013@gmail.com>
2025-12-17 10:33:39 +0200

    Fix: Hard prevent iOS pinch-zoom and double-tap via JS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Prevent zoom on iOS
+document.addEventListener('gesturestart', function (e) {
+  e.preventDefault();
+});
+
+// Prevent double-tap zoom
+let lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
